91d069502594c12639822ce05b6b4bbf3a81d046
Javier Caballero <caballerojavier13@gmail.com>
2025-03-24 14:17:53 -0400

    Update quote.svg.ts

--- pages/api/quote.svg.ts
+++ pages/api/quote.svg.ts
@@ -6,12 +6,12 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
   const { quote, author } = quotes[randomIndex];
 
   const svg = `
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 200">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 200">
        <rect fill="#f0f0f0"/>
-      <text x="20" y="40" font-family="Arial" font-size="16" fill="#333" text-anchor="start">
+      <text x="20" y="40" font-family="Arial" font-size="16" fill="#333" text-anchor="start" textLength="800">
         <tspan x="20" dy="0">${quote}</tspan>
       </text>
-      <text x="100%" y="12em" font-family="Arial" font-size="14" fill="#777" text-anchor="end">
+      <text x="780" y="8em" font-family="Arial" font-size="14" fill="#777" text-anchor="end">
         - ${author}
       </text>
     </svg>
