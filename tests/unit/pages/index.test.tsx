86ee6f6be59a09af9f8db08511a36d5b887d1212
Javier Caballero <caballerojavier13@gmail.com>
2025-03-26 16:30:51 -0400

    Update index.test.tsx

--- tests/unit/pages/index.test.tsx
+++ tests/unit/pages/index.test.tsx
@@ -5,6 +5,21 @@ import Home from '../../../pages/index';
 // Mock the fetch function
 global.fetch = jest.fn();
 
+// Mock window.matchMedia
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
 describe('Home', () => {
   it('renders a heading', () => {
     render(<Home />);
