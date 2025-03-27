1dbd42f928e8ec7b71fa52d3a518cf7efc258df5
Javier Caballero <caballerojavier13@gmail.com>
2025-03-27 16:08:02 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { Box, IconButton } from '@mui/material';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import RotateRightIcon from '@mui/icons-material/RotateRight';
 import Head from 'next/head';
-import ThemeProvider from '../src/components/ThemeProvider';
+import ThemeProvider, { useTheme } from '../src/components/ThemeProvider';
 import Loading from '../src/components/Loading';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
