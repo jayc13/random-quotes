3b642df641ae8c6fdd6e67ee6458683f63aa30de
Javier Caballero <javier.caballero@compass.com>
2025-03-29 16:57:43 -0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,24 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const compat = new FlatCompat();
+
 module.exports = [
   {
     ignores: [
-        '.github/**',
-        'node_modules/**',
-        'jest.config.js',
-        'package.json',
-        'package-lock.json',
-        'run_tests.sh',
-        'tsconfig.json',
-        'vercel.json',
-        'babel.config.js',
-        'next.config.js'
+      '.github/**',
+      'node_modules/**',
+      'jest.config.js',
+      'package.json',
+      'package-lock.json',
+      'run_tests.sh',
+      'tsconfig.json',
+      'vercel.json',
+      'babel.config.js',
+      'next.config.js'
     ],
   },
   {
     files: ["**/*.{js,jsx,ts,tsx}"],
-    extends: ["next", "next/core-web-vitals"],
+    ...compat.extends("next", "next/core-web-vitals"),
     parser: "@typescript-eslint/parser",
     parserOptions: {
       project: "./tsconfig.json",
