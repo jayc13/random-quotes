79d52be2138657153a1b9d2a6c9ac7d9839db8d2
Javier Caballero <javier.caballero@compass.com>
2025-03-29 18:18:04 -0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -24,3 +24,44 @@ const compat = new FlatCompat({
     allConfig: js.configs.all
 });
 export default [...compat.extends("next/core-web-vitals")];
+
+
+
+// @ts-check
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import { fixupConfigRules } from "@eslint/compat";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+const patchedConfig = fixupConfigRules([...compat.extends("next/core-web-vitals")]);
+
+const config = [
+    ...patchedConfig,
+    // Add more flat configs here
+    { 
+        ignores: [
+            ".next/*",
+            '.github/*',
+              'node_modelues/*',
+              'jest.config.js',
+              'package.json',
+              'package-lock.json',
+              'run_tests.sh',
+              'tsconfig.json',
+              'vercel.json',
+              'babel.config.js',
+              'next.config.js'
+        ],
+    },
+];
+
+export default config;
