798143feba16d184315a104b6849471574235262
Javier Caballero <javier.caballero@compass.com>
2025-03-29 18:32:49 -0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,3 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    ignores: [
-      '.github/**',
-      'node_modules/**',
-      '.next/**',
-      'jest.config.js',
-      'package.json',
-      'package-lock.json',
-      'run_tests.sh',
-      'tsconfig.json',
-      'vercel.json',
-      'babel.config.js',
-      'next.config.js'
-    ],
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-export default [...compat.extends("next/core-web-vitals")];
-
-
-
 // @ts-check
 import path from "node:path";
 import { fileURLToPath } from "node:url";
