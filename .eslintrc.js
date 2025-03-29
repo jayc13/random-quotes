bb65fe770c38fa0cd2215a79a2d6778a62cafbb1
Javier Caballero <caballerojavier13@gmail.com>
2025-03-29 16:46:41 -0400

    Update ESLint configuration to v9 flat config (#81)

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = [
+  {
+    ignores: [
+        '.github/**',
+        'node_modules/**',
+        'jest.config.js',
+        'package.json',
+        'package-lock.json',
+        'run_tests.sh',
+        'tsconfig.json',
+        'vercel.json',
+        'babel.config.js',
+        'next.config.js'
+    ],
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    extends: ["next", "next/core-web-vitals"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      project: "./tsconfig.json",
+    },
+  },
+];
